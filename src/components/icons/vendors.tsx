7696045aeac0feec4a69370fce4e44ee2b4cb653
YasiruShaminda <85274851+YasiruShaminda@users.noreply.github.com>
2025-08-13 15:38:43 +0530

    Create vendors.tsx

---
+++ src/components/icons/vendors.tsx
@@ -0,0 +1,23 @@
+import type { SVGProps } from "react";
+
+export const Cisco = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" {...props}>
+    <path d="M4 8h16" />
+    <path d="M4 12h16" />
+    <path d="M4 16h16" />
+  </svg>
+);
+
+export const Ruckus = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" {...props}>
+    <path d="M5 12s2.5-4 7-4 7 4 7 4-2.5 4-7 4-7-4-7-4z" />
+    <path d="M12 12a2 2 0 100-4 2 2 0 000 4z" />
+  </svg>
+);
+
+export const Actiontec = (props: SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" {...props}>
+        <path d="M10.6.9a2.3 2.3 0 012.8 0l8 6a2.3 2.3 0 011.4 2.1v10a2.3 2.3 0 01-2.3 2.3H3.5a2.3 2.3 0 01-2.3-2.3V9a2.3 2.3 0 011.4-2.1l8-6z" />
+        <path d="M12 21V9" />
+    </svg>
+);
